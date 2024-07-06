f44d379b56bddc6dffd9ab9f3d7aac6e9547309a
marigold0225 <mflovelky418@gmail.com>
2024-07-06 10:51:21 +0800

    add init config

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -9,4 +9,18 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.snippet.nvim-snippets" },
+
+  { import = "astrocommunity.indent.indent-blankline-nvim" },
+
+  { import = "astrocommunity.indent.mini-indentscope" },
+  { import = "astrocommunity.utility.noice-nvim" },
+
+  { import = "astrocommunity.markdown-and-latex.vimtex" },
+
+  { import = "astrocommunity.completion.cmp-cmdline" },
+
+  { import = "astrocommunity.motion.hop-nvim" },
 }
